b5dd7281fafe5c2fa3f0d6595ab68d4af1cb6278
Sunrock14 <farukkocademir@hotmail.com>
2024-12-25 00:27:05 +0300

    InitialCreate

---
+++ HybridCache.Api/Program.cs
@@ -0,0 +1,155 @@
+using Microsoft.Extensions.Caching.Hybrid;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddHybridCache();
+builder.Services.AddHybridCache(options =>
+{
+    options.MaximumPayloadBytes = 1024 * 1024;
+    options.MaximumKeyLength = 1024;
+    options.DefaultEntryOptions = new HybridCacheEntryOptions
+    {
+        Expiration = TimeSpan.FromMinutes(5),
+        LocalCacheExpiration = TimeSpan.FromMinutes(5)
+    };
+    //.AddSerializer<SomeProtobufMessage,
+    //    GoogleProtobufSerializer<SomeProtobufMessage>>();
+    //.AddSerializerFactory<GoogleProtobufSerializerFactory>();
+
+
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+public class SomeService(HybridCache cache)
+{
+    private HybridCache _cache = cache;
+
+    public async Task<string> GetSomeInfoAsync(string name, int id, CancellationToken token = default)
+    {
+        return await _cache.GetOrCreateAsync(
+            $"{name}-{id}", // Unique key to the cache entry
+            async cancel => await GetDataFromTheSourceAsync(name, id, cancel),
+            cancellationToken: token
+        );
+    }
+
+    public async Task<string> GetDataFromTheSourceAsync(string name, int id, CancellationToken token)
+    {
+        string someInfo = $"someinfo-{name}-{id}";
+        return someInfo;
+    }
+}
+public class SomeService1(HybridCache cache)
+{
+    private HybridCache _cache = cache;
+
+    public async Task<string> GetSomeInfoAsync(string name, int id, CancellationToken token = default)
+    {
+        return await _cache.GetOrCreateAsync(
+            $"{name}-{id}", // Unique key to the cache entry
+            (name, id, obj: this),
+            static async (state, token) =>
+            await state.obj.GetDataFromTheSourceAsync(state.name, state.id, token),
+            cancellationToken: token
+        );
+    }
+
+    public async Task<string> GetDataFromTheSourceAsync(string name, int id, CancellationToken token)
+    {
+        string someInfo = $"someinfo-{name}-{id}";
+        return someInfo;
+    }
+}
+
+public class SomeService2(HybridCache cache)
+{
+    private HybridCache _cache = cache;
+
+    public async Task<string> GetSomeInfoAsync(string name, int id, CancellationToken token = default)
+    {
+        var tags = new List<string> { "tag1", "tag2", "tag3" };
+        var entryOptions = new HybridCacheEntryOptions
+        {
+            Expiration = TimeSpan.FromMinutes(1),
+            LocalCacheExpiration = TimeSpan.FromMinutes(1)
+        };
+        return await _cache.GetOrCreateAsync(
+            $"{name}-{id}", // Unique key to the cache entry
+            async cancel => await GetDataFromTheSourceAsync(name, id, cancel),
+            entryOptions,
+            tags,
+            cancellationToken: token
+        );
+    }
+
+    public async Task<string> GetDataFromTheSourceAsync(string name, int id, CancellationToken token)
+    {
+        string someInfo = $"someinfo-{name}-{id}";
+        return someInfo;
+    }
+}
+
+public class SomeService4(HybridCache cache)
+{
+    private HybridCache _cache = cache;
+
+    public async Task<string> GetSomeInfoAsync(string name, int id, CancellationToken token = default)
+    {
+        var tags = new List<string> { "tag1", "tag2", "tag3" };
+        var entryOptions = new HybridCacheEntryOptions
+        {
+            Expiration = TimeSpan.FromMinutes(1),
+            LocalCacheExpiration = TimeSpan.FromMinutes(1)
+        };
+        return await _cache.GetOrCreateAsync(
+            $"{name}-{id}", // Unique key to the cache entry
+            async cancel => await GetDataFromTheSourceAsync(name, id, cancel),
+            entryOptions,
+            tags,
+            cancellationToken: token
+        );
+    }
+
+    public async Task<string> GetDataFromTheSourceAsync(string name, int id, CancellationToken token)
+    {
+        string someInfo = $"someinfo-{name}-{id}";
+        return someInfo;
+    }
+}
